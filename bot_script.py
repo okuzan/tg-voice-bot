067bdd053d5e2781b44d7da7188567b75fc5b351
okuzan <kuzanoleg@gmail.com>
2023-12-03 16:22:37 +0200

    python script added

---
+++ bot_script.py
@@ -0,0 +1,33 @@
+from telegram import InlineQueryResultVoice, InlineQuery, Update
+from telegram.ext import Application, InlineQueryHandler, ContextTypes
+
+BOT_TOKEN = '6964802840:AAFM2Km9WmY8htFVGhheQzUmwltRrZsFB-k'
+
+async def inline_query_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.inline_query.query
+
+    results = [
+        InlineQueryResultVoice(
+            id='1',
+            voice_url='https://github.com/okuzan/tg-voice-bot/blob/main/voices/IDGAF.ogg',
+            title='Похуй максимально'
+        ),
+        InlineQueryResultVoice(
+            id='2',
+            voice_url='http://example.com/your_voice_message2.ogg',
+            title='Pillow scream'
+        )
+    ]
+
+    await update.inline_query.answer(results)
+
+def main() -> None:
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    inline_query_handler = InlineQueryHandler(inline_query_handler)
+    application.add_handler(inline_query_handler)
+
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
